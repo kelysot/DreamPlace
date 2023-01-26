b3fc5417b832f794607b9d81c4bc1195badd6110
kelysot <kelysot@gmail.com>
2023-01-26 12:09:34 +0200

    Add path of gdal and geos to settings.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -155,3 +155,6 @@ DJOSER = {
     'USER_CREATE_PASSWORD_RETYPE': True,
     'SEND_ACTIVATION_EMAIL': False,
 }
+
+GDAL_LIBRARY_PATH = '/opt/homebrew/Cellar/gdal/3.6.2_2/lib/libgdal.dylib'
+GEOS_LIBRARY_PATH = '/opt/homebrew/Cellar/geos/3.11.1/lib/libgeos_c.dylib'
